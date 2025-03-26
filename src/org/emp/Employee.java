d063382d988eb80dbafc9cc1af724478bdc3b560
Frank0097 <franklin199703.r@gmail.com>
2025-03-26 16:51:17 +0530

    Project Structure

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,20 @@
+package org.emp;
+
+public class Employee 
+{
+	public void empId(int id)
+	{
+		System.out.println("enter id:" +id);
+	}
+
+	public static void main(String[] args) {
+	
+
+		Employee em=new Employee();
+		em.empId(1);
+		em.empId(2);
+		em.empId(3);
+		
+	}
+
+}
